5e1f85f0ff9cfde79ade72e11f88fa196d91395f
gplawinski <mcgregor1406@gmail.com>
2026-05-10 17:04:55 +0200

    Add chunk_base_terrain class and format tile selection logic in simplex island and sea chunks

--- neuroship/World/Terrain/Chunks/chunk_simplex_island.gd
+++ neuroship/World/Terrain/Chunks/chunk_simplex_island.gd
@@ -6,11 +6,11 @@ const DARK_GRASS_TILE := Vector2i(6, 1)
 const WATER_TILE := Vector2i(8, 0)
 
 func _get_tile_from_noise(noise_value: float) -> Vector2i:
-    if noise_value < 0.0:
-        return WATER_TILE
-    elif noise_value >= 0.0 and noise_value < 0.3:
-        return SAND_TILE
-    elif noise_value >= 0.3 and noise_value < 0.5:
-        return GRASS_TILE
-    else:
-        return DARK_GRASS_TILE
+	if noise_value < 0.0:
+		return WATER_TILE
+	elif noise_value >= 0.0 and noise_value < 0.3:
+		return SAND_TILE
+	elif noise_value >= 0.3 and noise_value < 0.5:
+		return GRASS_TILE
+	else:
+		return DARK_GRASS_TILE
