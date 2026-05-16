004aef89db8e1bdd0de56b66378a94c746a3b286
gplawinski <mcgregor1406@gmail.com>
2026-05-16 21:42:43 +0200

    Add biome data script

---
+++ neuroship/World/Terrain/biome_data.gd
@@ -0,0 +1,7 @@
+class_name BiomeData
+extends Resource
+
+@export var biome_name: String = "New Biome"
+@export var min_noise: float = -1.0
+@export var max_noise: float = 1.0
+@export var chunk_scenes: Array[PackedScene] = []
